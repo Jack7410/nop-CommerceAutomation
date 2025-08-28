dff8f418ad52de94a1fa90341a099a11ad91583f
Jack7410 <jpathan0606@gmail.com>
2025-08-28 23:35:14 +0530

    docs: add generated reports, log and screenshot

--- src/test/java/com/TestCases/BaseClass.java
+++ src/test/java/com/TestCases/BaseClass.java
@@ -1,10 +1,19 @@
 package com.TestCases;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -15,8 +24,10 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
+
 import com.PageObject.LoginPage;
 import com.Utilities.ReadProperties;
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import net.bytebuddy.agent.builder.AgentBuilder.InitializationStrategy.SelfInjection.Eager;
@@ -27,6 +38,7 @@ public class BaseClass {
 	
 	WebDriver driver; 
 	ReadProperties readProp;
+	Logger logger; 
 	
 	@Parameters("Browser")
 	@BeforeTest
@@ -34,28 +46,45 @@ public class BaseClass {
 	// Method to set the browser
 	// We can perform testing 1) Chrome 2)Firefox 3) Edge browser
 	void setBrowser(String browser) throws Throwable {
-		if(browser.equalsIgnoreCase("Chrome")) {
-			WebDriverManager.chromedriver().setup();
-			ChromeOptions opt =new ChromeOptions();
-			opt.setPageLoadStrategy(PageLoadStrategy.EAGER);
-			driver=new ChromeDriver(opt); 
-		}else if(browser.equalsIgnoreCase("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			FirefoxOptions opt=new FirefoxOptions(); 
-			opt.setPageLoadStrategy(PageLoadStrategy.EAGER);
-			driver=new FirefoxDriver(opt);
-		}else {
-			WebDriverManager.edgedriver().setup();
-			/*
-			 * EdgeOptions opt=new EdgeOptions(); opt.setPageLoadStrategy("EAGER");
-			 */
-			driver=new EdgeDriver();
-		}
 		
-		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-		driver.manage().window().maximize();
-		readProp=new ReadProperties(); 
-		driver.get(readProp.getUrl());
+		logger= LogManager.getLogger(this.getClass()); 
+		
+		
+		try {
+			if(browser.equalsIgnoreCase("Chrome")) {
+				
+				logger.info("*** Browser selected as "+browser);
+				WebDriverManager.chromedriver().setup();
+				ChromeOptions opt =new ChromeOptions();
+				opt.setPageLoadStrategy(PageLoadStrategy.EAGER);
+				driver=new ChromeDriver(opt); 
+			}else if(browser.equalsIgnoreCase("firefox")) {
+				logger.info("*** Browser selected as "+browser);
+				WebDriverManager.firefoxdriver().setup();
+				FirefoxOptions opt=new FirefoxOptions(); 
+				opt.setPageLoadStrategy(PageLoadStrategy.EAGER);
+				driver=new FirefoxDriver(opt);
+			}else {
+				logger.info("*** Browser selected as "+browser);
+				WebDriverManager.edgedriver().setup();
+				/*
+				 * EdgeOptions opt=new EdgeOptions(); opt.setPageLoadStrategy("EAGER");
+				 */
+				driver=new EdgeDriver();
+			}
+			
+			driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			readProp=new ReadProperties(); 
+			driver.get(readProp.getUrl());
+			logger.info("Url entered"+readProp.getUrl());
+		}catch(WebDriverException e){
+			logger.info("Unable to launch the brower");
+			logger.error(e.getMessage());
+			
+			
+		}
+
 		
 	}
 	
@@ -63,7 +92,23 @@ public class BaseClass {
 	// Method for close the browser
 	@AfterTest
 	void tearDown() {
+		
 		driver.close();
+		logger.info("Browser closed");
+		logger.info("****************************************************");
+	}
+	
+	public void takeScreenshot(WebDriver driver,String name) {
+		File sS=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		File path=new File(".//Screenshots/"+name+".png");
+		
+		try {
+			FileUtils.copyFile(sS, path);
+		} catch (IOException e) {
+			System.out.print("Screenshot not found: ");
+			e.printStackTrace();
+		} 
+		
 	}
 
 }
--- src/test/java/com/TestCases/LoginTest.java
+++ src/test/java/com/TestCases/LoginTest.java
@@ -4,20 +4,41 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.PageObject.LoginPage;
+import com.Utilities.ReadExcelData;
 
 public class LoginTest extends BaseClass {
 
 	@Test
 	void method() {
-		System.out.println("Pass");
+		
 		LoginPage lPage = new LoginPage(driver);
-		Assert.assertEquals(readProp.getLoginPageTitle(), driver.getTitle());
-
+		
+		try {
+			Assert.assertEquals(readProp.getLoginPageTitle(), driver.getTitle());
+			logger.info("Login Page tile matched");
+		}catch(AssertionError e) {
+			logger.error("Login page title mismatched getting following error :"+e);
+		}
+		
+		
+		takeScreenshot(driver, "method");
 		lPage.setUserName("admin@yourstore.com");
+		logger.info("User name entered");
 		lPage.setUserPass("admin");
+		logger.info("User password entered");
 		lPage.clickLoginBtn();
+		logger.info("Login btn clicked");
 
-		Assert.assertEquals(readProp.getHomePageTitle(), driver.getTitle());
+		try {
+			Assert.assertEquals(readProp.getHomePageTitle(), driver.getTitle());
+			logger.info("Loged in to application");
+		}catch (AssertionError e) {
+			logger.error("Login failed");
+			
+		}
+		
+		
+		
 	}
 
 }
